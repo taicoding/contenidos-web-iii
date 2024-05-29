e6ce7db309b146755451e6d3c7b583d125e0d4c5
taicoding <tai.delgadillo@gmail.com>
2024-05-29 04:26:51 -0400

    adding sample of 400

--- semana12/api_tdd/app/tests/test_user_controller.py
+++ semana12/api_tdd/app/tests/test_user_controller.py
@@ -4,6 +4,13 @@ def test_register_user(test_client):
     assert response.status_code == 201
 
 
+def test_register_duplicate_user(test_client):
+    new_user = {"username": "testuser", "password": "testpassword"}
+    response = test_client.post("/api/register", json=new_user)
+    assert response.status_code == 400
+    assert response.json["error"] == "El nombre de usuario ya está en uso"
+
+
 def test_login_user(test_client):
     user_credentials = {"username": "testuser", "password": "testpassword"}
     response = test_client.post("/api/login", json=user_credentials)
